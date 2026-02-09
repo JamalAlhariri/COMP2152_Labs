4c8c42db20e696212250020b2255b2b2dcc69807
alharirij9 <alharirij9@gmail.com>
2026-02-09 01:04:48 -0500

    add lab01

---
+++ Week01/lab01.py
@@ -0,0 +1,31 @@
+# Sample Coding Questions 01 Week 01
+# Name: Jamal Alhariri
+
+
+# Question 1: 
+myArray = [1, 4, 7, 9]
+print("Array values:", myArray)
+
+
+# Question 2
+a = 10
+b = 2
+c = 3
+d = 4
+
+e = (a - ((b ** c) // d)) + (a % c)
+print("Result of expression:", e)
+
+
+# Question 3
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+
+# Question 4
+addYears = 22
+userAge = int(input("Enter your age: "))
+userAge = userAge + addYears
+
+print("Now showing the shop items filtered by age:", userAge)
